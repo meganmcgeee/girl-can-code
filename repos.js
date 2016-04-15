53c14956e4d8a2dc4de28380940ffd0b734dc67d
meganmcgeee <meganpmcgee@gmail.com>
2016-04-14 22:44:58 -0700

    Changed styles, removed excess code, included links to other related/proposed projects to navigation

--- repos.js
+++ repos.js
@@ -11,9 +11,13 @@ function getWatchedRepos()
     	success : function ( returndata ) {
       	$.each( returndata.data, function ( i, item ) {
         	html += '<table>' +
+          // Name of the Repo
           	'<h2><a href="' + this.html_url + '">' + this.name + '</a></h2>' +
+            // Details of Repo
           	'<ul class="repo_details">' +
-            '<li id="owner">' + 'Owner: ' + this.owner.login + '</li><br>' +
+            // Owner of the Repo
+            '<li id="owner">' + 'Author: '  + this.owner.login + '</li><br>' +
+            // Language of the Repo
           	'<li id="language">' + 'Language: ' + this.language + '</li><br>' +
           	'</ul>' +
             '<hr>' +
